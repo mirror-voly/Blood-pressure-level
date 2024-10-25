e2cb3e7f0753f9755990850d52ca89242c06afd6
nur <77068320+mirror-voly@users.noreply.github.com>
2024-10-25 17:07:30 +0400

    Model added

---
+++ Blood pressure level/Model/Measurement.swift
@@ -0,0 +1,25 @@
+//
+//  Measurement.swift
+//  Blood pressure level
+//
+//  Created by mix on 25.10.2024.
+//
+
+import Foundation
+
+final class Measurement {
+	private let id = UUID()
+	private let systolicLevel: Int
+	private let diastolicLevel: Int
+	private let pulse: Int
+	private let date: Date
+	private let note: String
+	
+	init(systolicLevel: Int, diastolicLevel: Int, pulse: Int, date: Date, note: String) {
+		self.systolicLevel = systolicLevel
+		self.diastolicLevel = diastolicLevel
+		self.pulse = pulse
+		self.date = date
+		self.note = note
+	}
+}
